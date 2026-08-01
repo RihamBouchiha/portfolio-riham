6093f70aa2f3d98b9f752ee24a2721e99ccc59d8
riham <riham.bouchiha@usmba.ac.ma>
2026-08-01 01:30:51 +0100

    Add favicon SVG and update layout metadata for improved branding

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,18 @@
-import './globals.css'
+import './globals.css';
 
-// C'est ici qu'on définit le Favicon et le Titre de l'onglet
 export const metadata = {
   title: 'Riham Bouchiha - Portfolio',
   description: 'Portfolio de Riham Bouchiha',
   icons: {
-    icon: 'logo.png', // Assure-toi d'avoir ton image nommée favicon.ico dans le dossier 'public' ou 'app'
-    // Ou si tu as un PNG : icon: '/mon-logo.png',
+    icon: '/riham-favicon.svg',
+    shortcut: '/riham-favicon.svg',
   },
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      {/* On ajoute suppressHydrationWarning pour éviter les erreurs 
-        si une extension de navigateur modifie le code HTML 
-      */}
-      <body suppressHydrationWarning={true}>
-        {children}
-      </body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
-  )
+  );
 }
